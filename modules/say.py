a6e0dbe1b54ceabc5ea79308151aa572c062c34f
LordKorea <lk97798@posteo.net>
2019-06-13 11:53:28 +0200

    added feature to mention everyone, added feature to choose a channel

--- modules/say.py
+++ modules/say.py
@@ -4,6 +4,19 @@ from modules.module import Module
 
 class SayModule(Module):
 
+    def __init__(self, frontend, bot):
+        """Constructor."""
+        super().__init__(frontend, bot)
+        self._syntax = ("Syntax: .say COLOUR §§ TITLE §§ CONTENT\n\n"
+                        + "Optionally add fields at the end of the command:\n"
+                        + ".say 0xFF0000 §§ Test §§ Text §§ Field 1 §§ Desc 1"
+                        + " §§ Field 2 §§ Desc 2\n\n"
+                        + "Replace .say by .announce to ping `@everyone`.\n\n"
+                        + "In order to target another channel, append the"
+                        + " channel ID to the command:\n"
+                        + ".say743841223333 ...\n"
+                        + ".announce856823533737772 ...\n")
+
     async def on_command(self, msg, cmd, args):
         """Event handler for commands.
 
@@ -12,8 +25,17 @@ class SayModule(Module):
             cmd: The command label.
             args: The command arguments.
         """
-        if cmd != "say":
+        if not cmd.startswith("say") and not cmd.startswith("announce"):
             return
+        announce = True if cmd.startswith("announce") else False
+
+        channel = None
+        if announce:
+            if cmd != "announce":
+                channel = int(cmd[8:])
+        else:
+            if cmd != "say":
+                channel = int(cmd[3:])
 
         allow = False
         for role in msg.author.roles:
@@ -23,18 +45,15 @@ class SayModule(Module):
             log("Forbidden: .say for {0}".format(msg.author))
             return
 
-        if len(args) == 0:
-            embed = create_embed("Error - No arguments",
-                                 "Syntax: .say COLOR §§ TITLE §§ CONTENT",
+        argstr = " ".join(args)
+        realargs = argstr.split("§§")
+        if len(realargs) < 3:
+            embed = create_embed("Error - To few arguments", self._syntax,
                                  0xAA0000)
             await msg.channel.send(embed=embed)
             return
-
-        argstr = " ".join(args)
-        realargs = argstr.split("§§")
-        if len(realargs) != 3:
-            embed = create_embed("Error - To few real arguments",
-                                 "Syntax: .say COLOR §§ TITLE §§ CONTENT",
+        if len(realargs) % 2 != 1:
+            embed = create_embed("Error - Unbalanced fields", self._syntax,
                                  0xAA0000)
             await msg.channel.send(embed=embed)
             return
@@ -43,12 +62,24 @@ class SayModule(Module):
             color = int(realargs[0], 16)
             title = realargs[1]
             content = realargs[2]
+            data = {}
+            fieldlen = (len(realargs) -3) // 2
+            for i in range(fieldlen):
+                header = realargs[3 + i * 2]
+                desc = realargs[4 + i * 2]
+                data[header] = desc
         except:
-            embed = create_embed("Error - Parsing failed",
-                                 "Example: .say 0xFF0000 §§ Hey there! §§ OK?",
+            embed = create_embed("Error - Parsing failed", self._syntax,
                                  0xAA0000)
             await msg.channel.send(embed=embed)
             return
 
-        embed = create_embed(title, content, color)
-        await msg.channel.send(embed=embed)
+        tagline = ""
+        if announce:
+            tagline = "@everyone"
+        embed = create_embed(title, content, color, data)
+        if channel is None:
+            channel = msg.channel
+        else:
+            channel = msg.channel.guild.get_channel(channel)
+        await channel.send(tagline, embed=embed)
